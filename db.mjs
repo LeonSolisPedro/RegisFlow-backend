9cb56aeecd25cfd0eb5856bd4f5f3c3026555f4e
Pedro León <pedro@wintercr.com>
2024-09-21 16:53:39 -0500

    Updating variables names

--- db.mjs
+++ db.mjs
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise"
 
 const db = mysql.createPool({
-  host: process.env.HOST ?? "localhost",
-  user: process.env.USER ?? "root",
+  host: process.env.HOSTMYSQL ?? "localhost",
+  user: process.env.USERMYSQL ?? "root",
   password: process.env.PASSWORD ?? "my-secret-pw",
   database: process.env.DATABASE ?? "regisflow",
   timezone: 'Z'
